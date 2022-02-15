b218e86c077b7bdb995628050fe2791b397b4649
MartinK <82144965+RaikunGZ@users.noreply.github.com>
2022-02-15 11:30:59 +0100

    first Upload
    Init commit
    index.html, script.js, style.css, images (gb, img01, img02, img03)

---
+++ script.js
@@ -0,0 +1,56 @@
+// VERARBEITUNG
+
+const arrLogin = ["Martin", "Password123", "Nico", "Password222"]
+let loginName = "";
+let password = ""
+
+const login = () =>{
+    loginName = document.getElementById("login").value
+    password = document.getElementById("password").value
+    if(arrLogin.includes(loginName) && arrLogin.includes(password)){
+        document.getElementById("bmi").style.display = "flex";
+    }else{
+        console.log("ups")
+    }
+}
+
+const bmiCalc = ()=>{
+    const age = document.getElementById("age").value
+    const weight = document.getElementById("gewicht").value
+    const size =document.getElementById("groesse").value
+    
+    let resultBMI = weight/size**2*10000;
+    document.getElementById("bmiOutput");
+    document.getElementById("bmiScale")
+    if(resultBMI < 25){
+        document.getElementById("bmiOutput").style.backgroundColor = "green";
+        document.getElementById("bmiScale").style.backgroundColor = "green";
+    }else if(resultBMI < 29){
+        document.getElementById("bmiOutput").style.backgroundColor = "orange";
+        document.getElementById("bmiScale").style.backgroundColor = "orange";
+    }else{
+        document.getElementById("bmiOutput").style.backgroundColor = "red";
+        document.getElementById("bmiScale").style.backgroundColor = "red";
+    }
+    resultVisual = resultBMI.toFixed(2)*1.8 + "%";
+    console.log(resultVisual)
+    document.getElementById("bmiScale").style.width = resultVisual;
+    document.getElementById("bmiOutput").textContent = resultBMI.toFixed(2);
+    document.getElementById("bmiScale").textContent = resultBMI.toFixed(2)+"BMI";
+}
+
+let counter = 0;
+
+const changer = ()=>{
+    const arrIMG = ["./images/img01.png", "./images/img02.jpg", "./images/img03.jpg"]
+    document.getElementById("changer").src=arrIMG[counter]
+    if(counter < 2){
+    counter++
+    }else{
+    counter = 0;
+    }
+}
+
+
+changer();
+setInterval(changer, 5000);
