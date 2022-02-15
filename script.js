9dda57c9bc97706fe6f632ede2b23749e1dc71c6
MartinK <82144965+RaikunGZ@users.noreply.github.com>
2022-02-15 15:36:12 +0100

    last update
    last small updates
    changeable text
    h1
    small puffer between emelemnts on smaller devices

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const arrLogin = ["Martin", "Password123", "Nico", "Password222"]
 let loginName = "";
 let password = ""
 
+let tippGreen = "Du bist im einklang mit dir selbst und lebst gesund."
+let tippOrange = "Du solltets ein wenig auf dein gewicht achten."
+let tippRed = "Du solltest Sport treiben. Empfehlung: Schwimmen ist Gelenkschonend"
+
 const login = () =>{
     loginName = document.getElementById("login").value
     password = document.getElementById("password").value
@@ -25,12 +29,15 @@ const bmiCalc = ()=>{
     if(resultBMI < 25){
         document.getElementById("bmiOutput").style.backgroundColor = "green";
         document.getElementById("bmiScale").style.backgroundColor = "green";
+        document.getElementById("tipp").textContent = tippGreen
     }else if(resultBMI < 29){
         document.getElementById("bmiOutput").style.backgroundColor = "orange";
         document.getElementById("bmiScale").style.backgroundColor = "orange";
+        document.getElementById("tipp").textContent = tippOrange
     }else{
         document.getElementById("bmiOutput").style.backgroundColor = "red";
         document.getElementById("bmiScale").style.backgroundColor = "red";
+        document.getElementById("tipp").textContent = tippRed
     }
     resultVisual = resultBMI.toFixed(2)*1.8 + "%";
     console.log(resultVisual)
@@ -50,7 +57,6 @@ const changer = ()=>{
     counter = 0;
     }
 }
-
 
 changer();
 setInterval(changer, 5000);
